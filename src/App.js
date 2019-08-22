1c30feedc39ec6f7f42ed11e02875c033394a010
Aaron <nordin.aaron@gmail.com>
2019-08-22 16:05:12 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import CircleWave from "./CircleWave";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <svg
+        width="960"
+        height="500"
+        // angles={d3.range(0, 2 * Math.PI, Math.PI / 200)}
+      >
+        <CircleWave
+        width={960}
+        height={500}
+        // angles={d3.range(0, 2 * Math.PI, Math.PI / 200)}
+        />
+      </svg>
     </div>
   );
 }
